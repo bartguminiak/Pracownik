5e6e665447bef1eb40af8b20140a98bc5e3f3d9a
Bartlomiej <guminiak.bartlomiej@gmail.com>
2018-10-05 12:08:01 +0200

    refactor

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
   describe '#full_name' do
-    let(:employee) { FactoryBot.create(:employee, first_name: 'Donald', last_name: 'Trump') }
+    let(:employee) { FactoryBot.build(:employee, first_name: 'Donald', last_name: 'Trump') }
 
     it 'shoud return full name of employee' do
       expect(employee.full_name).to eq('Donald Trump')
