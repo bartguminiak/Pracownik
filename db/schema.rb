178311e8dd25ac3b4854fae61e95f963fc787932
Bartlomiej <guminiak.bartlomiej@gmail.com>
2018-10-05 15:40:21 +0200

    add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_140647) do
+ActiveRecord::Schema.define(version: 2018_10_05_131033) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
